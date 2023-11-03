8f5580cb148f3cd4d96a55d15106b9501adea562
Calc Calc <calc@list.ru>
2023-11-04 02:45:01 +0300

    [*] delay reset

--- core/src/main/kotlin/su/bnet/zombie/utility/Delay.kt
+++ core/src/main/kotlin/su/bnet/zombie/utility/Delay.kt
@@ -1,13 +1,17 @@
 package su.bnet.zombie.utility
 
 class Delay(
-    private val delay: Float,
+    var delay: Float,
     private val onRun: () -> Unit
 ) {
     private var currentDelay = 0f
 
     private val isLock
         get() = currentDelay > 0f
+
+    fun reset(reset: Float = 0f) {
+        currentDelay = reset
+    }
 
     fun update(deltaTime: Float) {
         currentDelay -= deltaTime
